6dd2b215ff0853186137360fbac752d92b53e63b
Alisa <alisa.blakeney@gmail.com>
2021-02-21 11:57:17 +1100

    add save favourite functionality

--- src/List.js
+++ src/List.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import Loader from './Loader.js'
 
 export default function List () {
 
     const [items, setItems] = useState()
+    const [faves, setFaves] = useState([])
     const [loading, setLoading] = useState(true)
+    const [refresh, setRefresh] = useState();
 
     useEffect(() => {
         fetch(`https://api.kanye.rest`)
         .then(res=>res.json())
         .then(data=>{
-            console.log(data)
+            console.log("fetched data",data)
             if(data && !data.error){
                 setItems(data)
             }
@@ -17,11 +20,27 @@ export default function List () {
         .catch(console.log)
         .finally(setLoading(false))
 
-    }, [])
+    }, [refresh])
+
+    const handleButtonClick = () => {
+        setRefresh({});
+    }
+
+    const handleAddFave = () => {
+        // setFaves([items])
+        // console.log("currrent quote", items)
+        // console.log("faves",faves)
+        setFaves([...faves, items])
+        setRefresh({});
+    }
 
     return (<div className="list">
-        {/* {loading && <Loader />} */}
+        {loading && <Loader />}
         {items && <h1>{items.quote}</h1>}
+        <button onClick={handleButtonClick}>Next</button>
+        <button onClick={handleAddFave}>Add to favourites</button>
+        <h1>Faves</h1>
+        <ul>{faves && faves.map(fave=><li>{fave.quote}</li>)}</ul>
     </div>
     )
 }
